498dbfdec427eba7d77d15bdc498fd402e95574f
William Brown <william@blackhats.net.au>
2021-02-12 13:51:43 +1000

    Add ability to autodetect licenses for RPM

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use serde_derive::Deserialize;
 use std::env;
+use std::path::Path;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -10,7 +11,11 @@ struct Opt {
     #[structopt(parse(from_os_str))]
     _dummy: PathBuf,
     #[structopt(parse(from_os_str))]
-    input: Option<PathBuf>,
+    /// The directory containing the Cargo.toml.
+    workdir: Option<PathBuf>,
+    #[structopt(parse(from_os_str))]
+    /// The path to the associated vendor directory.
+    vendordir: Option<PathBuf>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -27,15 +32,83 @@ struct Config {
     package: Vec<Pkg>,
 }
 
+#[derive(Debug, Deserialize)]
+struct CargoPkg {
+    license: Option<String>,
+    #[serde(rename = "license-file")]
+    license_file: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Cargotoml {
+    package: CargoPkg,
+}
+
+fn do_license_check(vendordir: &Path, debug: bool) -> Option<String> {
+    let name = vendordir.join("Cargo.toml");
+    // https://doc.rust-lang.org/cargo/reference/manifest.html#the-license-and-license-file-fields
+    if debug {
+        eprintln!("checking license in ... {:?}", name);
+    }
+    if !name.exists() {
+        eprintln!(
+            "Unable to check license from {:?}. You may need to check this manually",
+            name
+        );
+        return None;
+    }
+
+    let buffer = std::fs::read(&name).expect("Unable to open cargo.toml for reading!");
+
+    let config: Cargotoml =
+        toml::from_slice(&buffer).expect("Unable to parse cargo.toml, invalid!");
+
+    if debug {
+        eprintln!("Parsed config - {:?}", config);
+    }
+
+    match (config.package.license, config.package.license_file) {
+        (Some(mut lic), _) => {
+            // If it contains an operator, we need braces.
+            if lic.contains("OR") || lic.contains("AND") {
+                lic.insert_str(0, "( ");
+                lic.push_str(" )");
+            }
+            Some(lic)
+        }
+        (None, Some(fname)) => {
+            let license_file = vendordir.join(fname);
+            eprintln!(
+                "Unable to find license in {:?}. You may need to check {:?} for details.",
+                name, license_file
+            );
+            None
+        }
+        (None, None) => {
+            eprintln!(
+                "Unable to determine license for {:?}. You must manually investigate!",
+                name
+            );
+            None
+        }
+    }
+}
+
 fn main() {
     let opt = Opt::from_args();
 
     let path = opt
-        .input
+        .workdir
         .unwrap_or_else(|| env::current_dir().expect("Unable to locate current work dir"));
+
+    let vendordir = opt
+        .vendordir
+        .map(|s| PathBuf::from(s))
+        .unwrap_or_else(|| path.join("vendor"));
 
     if opt.debug {
         eprintln!("DEBUG -> working dir {:?}", path);
+        eprintln!("DEBUG -> vendor dir {:?}", vendordir);
     }
 
     // Do we have the Cargo.lock?
@@ -53,6 +126,28 @@ fn main() {
     let config: Config =
         toml::from_slice(&buffer).expect("Unable to parse lockfile, invalid toml!");
 
+    // Now check the licenses if possible.
+    let debug = opt.debug;
+    let mut licenses: Vec<String> = if vendordir.exists() {
+        if debug {
+            eprintln!("DEBUG -> found {:?}", vendordir);
+        }
+        config
+            .package
+            .iter()
+            .filter_map(|pkg| {
+                let pkg_vendored_path = vendordir.join(&pkg.name);
+                do_license_check(&pkg_vendored_path, debug)
+            })
+            .collect()
+    } else {
+        eprintln!("ERROR could not find vendordir - {:?}", vendordir);
+        Vec::new()
+    };
+
+    licenses.sort();
+    licenses.dedup();
+
     // Now output the values.
     if opt.debug {
         for pkg in &config.package {
@@ -63,6 +158,13 @@ fn main() {
     for pkg in &config.package {
         println!("Provides: bundled(crate({})) = {}", pkg.name, pkg.version);
     }
+
+    let mut license = String::new();
+    for lic in licenses.iter() {
+        license.push_str(&lic);
+        license.push_str(" AND ");
+    }
+    println!("License: {}", license);
 
     if opt.debug {
         eprintln!("DEBUG -> Success! 🎉");
